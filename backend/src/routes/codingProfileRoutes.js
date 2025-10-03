e86b98fdc9204d7b5aa801cd658c3ae9fe8d1072
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-10-03 11:31:23 +0530

    Delete backend directory

--- backend/src/routes/codingProfileRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createCodingProfile, getAllCodingProfiles } from "../controllers/codingProfileController.js";
-
-const router = express.Router();
-
-router.post("/", createCodingProfile);
-router.get("/", getAllCodingProfiles);
-
-export default router;
--- backend/src/routes/skillRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createSkill, getAllSkills } from "../controllers/skillController.js";
-
-const router = express.Router();
-
-router.post("/", createSkill);
-router.get("/", getAllSkills);
-
-export default router;
